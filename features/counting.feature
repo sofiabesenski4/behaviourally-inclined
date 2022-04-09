64a09ac01d3ed22cb64f205aa20439911e17679e
Sofia Besenski <sofia@super.gd>
2022-04-09 13:05:51 -0700

    Building out better assertions, new lib
    Introducing hamcrest matchers. We saw this explicitly in GOOS, and I
    think RSpec pretty much handles these out of the box.
    
    Also discovered that variables assigned to the context within some given
    examples, will persist unless overriden by the other examples.

--- features/counting.feature
+++ features/counting.feature
@@ -1,14 +1,20 @@
 Feature: showing off behave
 
   Scenario: Using the counter
-     Given starting at 0 with a step size of 1 
-      When we increment 1 time(s)
-      Then we should see 1. 
+    Given a counter starting at 1 
+    And with a step size of 1 
+    When we increment 1 time(s)
+    Then we should see 2
 
-     Given starting at 2 with a step size of 3
-     When we increment 2 time(s)
-     Then we should see 8
+    Given a counter starting at 2
+    And with a step size of 3
+    When we increment 2 time(s)
+    Then we should see 8
 
-     Given starting at 0 with a step size of -1
-     When we increment 1 time(s)
-     Then we should see -1.
+    Given a counter starting at 0 
+    When we increment 1 time(s)
+    Then we should see 1 
+
+    Given a counter starting at 0
+    When we increment 2 time(s)
+    Then we should see 2 
--- features/steps/counting.py
+++ features/steps/counting.py
@@ -1,14 +1,36 @@
 from behave import *
+from hamcrest import assert_that, equal_to, is_not
 # Must manually import the code under test in these steps.
 import models 
 
 # The context object trickles down through the steps, accumulating
 # state which we prepare-act-assert on.
-@given('starting at {start} with a step size of {step_size}')
-def step_impl(context, start, step_size):
+@given('a counter starting at {start}')
+def step_impl(context, start):
     context.start = start
+    context.counter = models.Incrementor(start = int(start)) 
+    # Without the following line, the context.step_size variable persists between
+    # the 
+    """
+  Given a counter starting at 0
+  And with a step size of -1
+  When we increment 1 time(s)
+  Then we should see -1
+
+  Given a counter starting at 0
+  When we increment 2 time(s)
+  Then we should see 2
+    """
+    #examples.
+    context.step_size = context.counter.step_size
+
+@given('with a step size of {step_size}')
+def step_impl(context, step_size):
     context.step_size = step_size
-    context.counter = models.Incrementor(start = start, step_size = step_size) 
+    if context.counter:
+        context.counter.step_size = int(step_size)
+    else:
+        context.counter = models.Incrementor(step_size = int(step_size))
 
 @when('we increment {count} time(s)')
 def step_impl(context, count):
@@ -17,4 +39,6 @@ def step_impl(context, count):
 
 @then('we should see {result}')
 def step_impl(context, result):
-    assert int(context.counter.current) == int(context.start) + int(context.step_size)*int(context.count)
+    print(context.counter)
+    expected_val = int(context.start) + int(context.step_size)*int(context.count)
+    assert_that(int(context.counter.current), equal_to(expected_val))
